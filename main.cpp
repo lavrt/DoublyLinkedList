a80dd67369fb30a80fde54823d938e06e529624e
lavrt <lavrik-aleksey-1@yandex.ru>
2024-11-02 18:20:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 #include <string.h>
 
 const size_t kSize = 10;
-const int POISON_1 = -333;
-const int POISON_2 = -1;
+const int POISON = -333;
+const int ptrPOISON = -1;
 const char* const kDumpFileName = "dump.gv";
 
 #define FCLOSE(ptr_) \
@@ -20,6 +20,16 @@ struct dblLinkedList
     size_t counter;
 };
 
+enum ErrorCodes
+{
+    Success                = 0,
+    ListOverflow           = 1,
+    ListUnderflow          = 2,
+    OutOfBoundsListAccess  = 3,
+    DumpFileOpenningError  = 4,
+    NullPointerDereference = 5,
+};
+
 void ctor(dblLinkedList* const list);
 void dtor(dblLinkedList* const list);
 void dump(const dblLinkedList* const list);
@@ -32,6 +42,8 @@ int front(const dblLinkedList* const list);
 int back(const dblLinkedList* const list);
 int next(const dblLinkedList* const list, const size_t index);
 int prev(const dblLinkedList* const list, const size_t index);
+int getNth(const dblLinkedList* const list, const size_t index);
+int search(const dblLinkedList* const list, int value);
 
 void deleteNth(dblLinkedList* const list, const size_t index);
 void clear(dblLinkedList* const list);
@@ -42,15 +54,15 @@ int main()
 
     ctor(&list);
 
-    // pushFront(&list, 10); pushFront(&list, 20); pushFront(&list, 30); pushFront(&list, 40); pushFront(&list, 50);
+    pushFront(&list, 10); pushFront(&list, 20); pushFront(&list, 30); pushFront(&list, 40); pushFront(&list, 50);
     // pushBeforeNth(&list, 1, 60); pushAfterNth(&list, 5, 52);
     // pushFront(&list, 5); pushFront(&list, 2); pushFront(&list, 52);
     // pushBack(&list, 10); pushBack(&list, 20); pushBack(&list, 30); pushBack(&list, 40);
     // pushBack(&list, 5); pushBack(&list, 2); pushBack(&list, 52);
-    // pushAfterNth(&list, 1, 52); pushAfterNth(&list, 1, 53);
+    pushAfterNth(&list, 1, 52); pushAfterNth(&list, 1, 53);
     // pushAfterNth(&list, 1, 152);
     // pushBeforeNth(&list, 1, 52); pushBeforeNth(&list, 1, 53);
-    pushBeforeNth(&list, 1, 777);
+    // pushBeforeNth(&list, 1, 777);
     // deleteNth(&list, 5);
 
     dump(&list);
@@ -66,13 +78,13 @@ void ctor(dblLinkedList* const list)
 
     list->free = 1;
     list->counter = 0;
-    list->data[0] = POISON_1;
+    list->data[0] = POISON;
 
     for (size_t i = 1; i < kSize; i++)
     {
-        list->data[i] = POISON_1;
-        list->next[i] = i + 1;
-        list->prev[i] = POISON_2;
+        list->data[i] = POISON;
+        list->next[i] = (int)i + 1;
+        list->prev[i] = ptrPOISON;
     }
     list->next[kSize - 1] = 0;
 }
@@ -94,13 +106,14 @@ void pushFront(dblLinkedList* const list, const int value)
     assert(list);
     assert(++list->counter < kSize);
 
+    int free = list->next[list->free];
     list->data[list->free] = value;
     list->next[list->free] = list->next[0];
     list->prev[list->next[0]] = list->free;
     list->next[0] = list->free;
     list->prev[list->free] = 0;
 
-    list->free++;
+    list->free = free;
 }
 
 void pushBack(dblLinkedList* const list, const int value)
@@ -108,43 +121,46 @@ void pushBack(dblLinkedList* const list, const int value)
     assert(list);
     assert(++list->counter < kSize);
 
+    int free = list->next[list->free];
     list->data[list->free] = value;
     list->next[list->prev[0]] = list->free;
     list->prev[list->free] = list->prev[0];
     list->next[list->free] = 0;
     list->prev[0] = list->free;
 
-    list->free++;
+    list->free = free;
 }
 
 void pushAfterNth(dblLinkedList* const list, const size_t index, const int value)
 {
     assert(list);
     assert(++list->counter < kSize);
-    assert(list->prev[index] != POISON_2);
+    assert(list->prev[index] != ptrPOISON);
 
+    int free = list->next[list->free];
     list->data[list->free] = value;
     list->next[list->free] = list->next[index];
     list->prev[list->next[index]] = list->free;
     list->next[index] = list->free;
     list->prev[list->free] = (int)index;
 
-    list->free++;
+    list->free = free;
 }
 
 void pushBeforeNth(dblLinkedList* const list, const size_t index, const int value)
 {
     assert(list);
     assert(++list->counter < kSize);
-    assert(list->prev[index] != POISON_2);
+    assert(list->prev[index] != ptrPOISON);
 
+    int free = list->next[list->free];
     list->data[list->free] = value;
     list->next[list->free] = (int)index;
     list->prev[list->free] = list->prev[index];
     list->next[list->prev[index]] = list->free;
     list->prev[index] = list->free;
 
-    list->free++;
+    list->free = free;
 }
 
 int front(const dblLinkedList* const list)
@@ -164,6 +180,7 @@ int back(const dblLinkedList* const list)
 int next(const dblLinkedList* const list, const size_t index)
 {
     assert(list);
+    assert(list->prev[index] != ptrPOISON);
 
     return list->next[index];
 }
@@ -171,20 +188,45 @@ int next(const dblLinkedList* const list, const size_t index)
 int prev(const dblLinkedList* const list, const size_t index)
 {
     assert(list);
+    assert(list->prev[index] != ptrPOISON);
 
     return list->prev[index];
+}
+
+int getNth(const dblLinkedList* const list, const size_t index)
+{
+    assert(list);
+    assert(list->prev[index] != ptrPOISON);
+
+    return list->data[index];
+}
+
+int search(const dblLinkedList* const list, int value)
+{
+    assert(list);
+
+    for (int i = list->next[0]; i != list->prev[0]; i = list->next[i])
+    {
+        if (list->data[i] == value)
+        {
+            return i;
+        }
+    }
+
+    return -1;
 }
 
 void deleteNth(dblLinkedList* const list, const size_t index)
 {
     assert(list);
     assert(list->counter--);
+    assert(list->prev[index] != ptrPOISON);
 
     list->next[list->prev[index]] = list->next[index];
     list->prev[list->next[index]] = list->prev[index];
 
-    list->next[index] = POISON_2;
-    list->prev[index] = POISON_2;
+    list->next[index] = ptrPOISON;
+    list->prev[index] = ptrPOISON;
 }
 
 void clear(dblLinkedList* const list)
